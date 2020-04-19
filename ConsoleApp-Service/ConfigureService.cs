073684f46b029eda8b5f111a751134d01c937065
ayitech <charles@ayitech.com>
2020-04-19 11:20:09 -0500

    Initial commit...

---
+++ ConsoleApp-Service/ConfigureService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace ConsoleApp_Service
+{
+    internal static class ConfigureService
+    {
+        internal static void Configure()
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<SimpleTimerService>(s =>
+                {
+                    s.ConstructUsing(timerservice => new SimpleTimerService());
+                    s.WhenStarted(timerservice => timerservice.Start());
+                    s.WhenStopped(timerservice => timerservice.Stop());
+                });
+
+                //Set account that windows service to run at
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("ConsoleApp-Service");
+                x.SetDisplayName("Ayitech Simple Timer Windows Service");
+                x.SetDescription("This is a simple timer windows service.");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
---
+++ ConsoleApp-Service/SimpleTimerService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ConsoleApp_Service
+{
+    public class SimpleTimerService
+    {
+        private readonly Timer _timer;
+
+        public SimpleTimerService()
+        {
+            _timer = new Timer(5000) { AutoReset = true };
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            // write code here that runs when the time elapsed
+            string[] lines = new string[] { DateTime.Now.ToString() + ": A message from Simple Timer Service..." };
+            File.AppendAllLines(@"C:\Temp\Demo\SimpleTimerService.txt", lines);
+        }
+
+        public void Start()
+        {
+            // write code here that runs when the Windows Service starts up. 
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            // write code here that runs when the Windows Service stops.
+            _timer.Stop();
+        }
+
+    }
+}
